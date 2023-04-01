6fb3af88e630bb197c841d972126022b0064c3fa
David Sn <divad.nnamtdeis@gmail.com>
2023-04-01 17:35:05 +0200

    feat: Add models and script to generate database

---
+++ generatedb.py
@@ -0,0 +1,190 @@
+import os
+import re
+
+import aiohttp
+from pylife_api import PylifeAPIClient
+from shapely.geometry import box
+from shapely.ops import unary_union
+from tortoise import Tortoise, run_async
+
+from mapapylife.models import Blip, House, Player, Organization, Zone
+
+MTA_ZONENAMES = "https://github.com/multitheftauto/mtasa-blue/raw/master/Shared/mods/deathmatch/logic/CZoneNames.cpp"
+ZONE_REGEX = r"{(-?\d+), (-?\d+), (?:-?\d+), (-?\d+), (-?\d+), (?:-?\d+), \"(.*?)\"}"
+
+# https://github.com/multitheftauto/mtasa-blue/blob/master/Shared/mods/deathmatch/logic/CZoneNames.cpp#L15-L24
+CITY_NAMES = [
+    "Tierra Robada",
+    "Bone County",
+    "Las Venturas",
+    "San Fierro",
+    "Red County",
+    "Whetstone",
+    "Flint County",
+    "Los Santos",
+]
+
+# Define exceptions for zones
+exceptions = {
+    # https://gta.fandom.com/pl/wiki/Mulholland
+    102: 90,
+}
+
+
+async def generate_zones():
+    print("Generating table for zones...")
+    zones = {}
+
+    # Load zone names from file
+    with open("data/zonenames.txt", encoding="utf-8") as f:
+        for line in f.readlines():
+            zone_id = int(line.split(",")[0])
+            zones[zone_id] = {
+                "name": line.split(",")[1],
+                "description": line.split(",")[2].rstrip(),
+                "points": []
+            }
+
+    # Download zone data from MTA repository
+    async with aiohttp.ClientSession() as session:
+        async with session.get(MTA_ZONENAMES) as resp:
+            resp.raise_for_status()
+            response = await resp.text()
+
+            for line in response.splitlines():
+                matches = re.findall(ZONE_REGEX, str(line))
+                if matches:
+                    row = matches[0]
+                    zone_id = next((index for index, zone in zones.items() if zone["name"] == row[4]))
+                    zones[zone_id]["points"].append(box(int(row[0]), int(row[1]), int(row[2]), int(row[3])))
+
+    # Now we iterate through every zone and save to database
+    for index, zone in zones.items():
+        union = unary_union(zone["points"])
+
+        # Check if union has more than one polygon
+        if union.geom_type == "MultiPolygon":
+            points = [[(int(point[0]), int(point[1])) for point in polygon.exterior.coords] for polygon in union.geoms]
+        else:
+            points = [(int(point[0]), int(point[1])) for point in union.exterior.coords]
+
+        await Zone.create(id=index, name=zone["name"], description=zone["description"], points=points)
+
+
+async def generate_root_zone_ids():
+    # First we load city zones
+    city_zones = {
+        zone.id: zone.get_polygon()
+        for zone in await Zone.filter(name__in=CITY_NAMES).order_by("id").distinct()
+    }
+
+    # Define function to calculate area
+    def calculate_area(z1, z2):
+        return z1.intersection(z2).area / z1.area
+
+    # Now we iterate though every zone and check which city zone it belongs
+    for zone in await Zone.filter(name__not_in=CITY_NAMES).order_by("id").distinct():
+        polygon = zone.get_polygon()
+
+        # Check if zone is in exceptions
+        if zone.id in exceptions:
+            zone.root_id = exceptions[zone.id]
+        else:
+            for city_id, city_polygon in city_zones.items():
+                if polygon.intersects(city_polygon) and calculate_area(polygon, city_polygon) > 0.5:
+                    zone.root_id = city_id
+                    break
+
+        await zone.save()
+
+
+async def generate_organizations(client: PylifeAPIClient):
+    print("Generating table for organizations...")
+
+    # Pull organizations from API and save them to database
+    for organization in await client.get_organizations():
+        await Organization.create(
+            id=organization.id,
+            name=organization.name,
+            tag=organization.tag,
+            logo_url=organization.logo,
+            registered=organization.registered,
+        )
+
+
+async def generate_players(client: PylifeAPIClient):
+    print("Generating table for players...")
+
+    # Pull players from API and save them to database
+    for player in await client.get_players():
+        await Player.create(
+            id=player.id,
+            name=player.login,
+            premium=player.premium,
+            registered=player.registered,
+            last_online=player.last_online,
+        )
+
+
+async def generate_houses(client: PylifeAPIClient):
+    print("Generating table for houses...")
+
+    # Pull houses from API and save them to database
+    for house in await client.get_houses():
+        # Get zone from house position
+        zone = await Zone.get_zone(house.position.x, house.position.y)
+
+        await House.create(
+            id=house.id,
+            x=house.position.x,
+            y=house.position.y,
+            name=house.title,
+            location_id=zone.id,
+            owner_id=house.owner,
+            organization_id=house.organization,
+            price=house.price,
+            expires=house.expires,
+        )
+
+
+async def generate_blips():
+    print("Generating table for blips...")
+
+    # Load blips from file
+    with open("data/blips.txt", encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.rstrip()
+
+            if line == "" or line.startswith("#"):
+                continue
+
+            blip = line.split(",")
+            await Blip.create(x=blip[0], y=blip[1], name=blip[2], icon=blip[3])
+
+
+async def run():
+    db_url = os.getenv("DB_URL", "sqlite://db.sqlite3")
+
+    # Connect to database and generate schemas
+    await Tortoise.init(db_url=db_url, modules={"models": ["mapapylife.models"]})
+    await Tortoise.generate_schemas()
+
+    # Generate zones
+    await generate_zones()
+    await generate_root_zone_ids()
+
+    # Connect to Pylife API and generate tables
+    async with PylifeAPIClient(auth_token=os.environ["AUTH_TOKEN"]) as client:
+        await generate_organizations(client)
+        await generate_players(client)
+        await generate_houses(client)
+
+    # Generate blips
+    await generate_blips()
+
+    # Close database connection
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(run())
---
+++ mapapylife/models.py
@@ -0,0 +1,104 @@
+from functools import lru_cache
+from typing import Union
+
+from shapely import Point, Polygon, MultiPolygon
+from tortoise.models import Model
+from tortoise import fields
+
+
+class House(Model):
+    id = fields.IntField(pk=True)
+    x = fields.FloatField(null=False)
+    y = fields.FloatField(null=False)
+    name = fields.CharField(max_length=255, null=False)
+    location = fields.ForeignKeyField("models.Zone", null=False)
+    owner = fields.ForeignKeyField("models.Player", on_delete=fields.SET_NULL, null=True)
+    organization = fields.ForeignKeyField("models.Organization", on_delete=fields.SET_NULL, null=True)
+    price = fields.FloatField(null=True)
+    expires = fields.DatetimeField(null=True)
+    last_update = fields.DatetimeField(null=False, auto_now=True)
+
+    class Meta:
+        table = "houses"
+
+    def __str__(self):
+        return f"{self.id}. {self.name}"
+
+
+class Blip(Model):
+    id = fields.IntField(pk=True)
+    x = fields.FloatField(null=False)
+    y = fields.FloatField(null=False)
+    name = fields.CharField(max_length=255, null=False)
+    icon = fields.CharField(max_length=255, null=False)
+
+    class Meta:
+        table = "blips"
+
+    def __str__(self):
+        return self.name
+
+
+class Zone(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255, null=False)
+    description = fields.CharField(max_length=255, null=False)
+    points = fields.JSONField(null=False)
+    root = fields.ForeignKeyField("models.Zone", null=True)
+
+    class Meta:
+        table = "zones"
+
+    @lru_cache
+    def get_polygon(self) -> Union[Polygon, MultiPolygon]:
+        if len(self.points[0]) == 2:
+            polygon = Polygon(self.points)
+        else:
+            polygon = MultiPolygon([Polygon(polygon) for polygon in self.points])
+
+        return polygon
+
+    def __contains__(self, item):
+        return self.get_polygon().contains(item)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    async def get_zone(x: float, y: float) -> "Zone":
+        point = Point(x, y)
+        city_zone = next(zone for zone in await Zone.filter(root_id__isnull=True) if point in zone)
+
+        for zone in await Zone.filter(root_id=city_zone.id):
+            if point in zone:
+                return zone
+
+        return city_zone
+
+
+class Player(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=22, null=False)
+    premium = fields.DatetimeField(null=True)
+    registered = fields.DatetimeField(null=False)
+    last_online = fields.DatetimeField(null=False)
+
+    class Meta:
+        table = "players"
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255, null=False)
+    tag = fields.CharField(max_length=255, null=False)
+    logo_url = fields.TextField(null=True)
+    registered = fields.DatetimeField(null=False)
+
+    class Meta:
+        table = "organizations"
+
+    def __str__(self):
+        return self.name
